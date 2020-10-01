c9c82eaace13756253978c40af24fba31591a598
hazedrops <hazedrops@hotmail.com>
2020-09-30 19:33:51 -0700

    Update settings.py

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f&nci0*cvjwwy*0=j)9rcm14=^cb2z!g8=^_&5n12_6#y)!bk9'
+SECRET_KEY = 'My_Secret_Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
